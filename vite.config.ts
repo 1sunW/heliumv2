5609cb01afbdc17bf501b1459e2f3a669bb98204
ace <149412950+1sunW@users.noreply.github.com>
2026-04-30 19:35:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'production' ? '/heliumv2/' : './',
+    base: './',
+    build: {
+      assetsDir: 'assets',
+    },
     plugins: [
       react(), 
       tailwindcss(),
@@ -16,7 +20,7 @@ export default defineConfig(({mode}) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.'),
       },
     },
     server: {
